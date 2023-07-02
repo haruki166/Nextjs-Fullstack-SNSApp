88a8475b478388fea418f789dda56e95d9814d20
haruki suziuki <harukiti166@gmail.com>
2023-07-02 15:37:27 +0900

    tokenを確認するミドルウェア

---
+++ api/middlewares/isAuthenticated.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+function isAuthenticated(req, res, next) {
+  //トークン取得
+  //apiClient.defaults.headers["Authorization"] = `Bearer ${token}`の${token}の部分を取得している
+  const token = req.headers.authorization?.split(" ")[1];
+
+  //トークンがなかったらエラー
+  if (!token) {
+    return res.status(401).json({ message: "権限がありません" });
+  }
+
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "権限がありません" });
+    }
+    
+    req.userId = decoded.id;
+
+
+    next();
+
+  });
+}
+
+module.exports = isAuthenticated;
