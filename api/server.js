cd9b238486bbd1571de003a81c2573b6a6c42efe
haruki suziuki <harukiti166@gmail.com>
2023-07-02 16:33:44 +0900

    route設定追加

--- api/server.js
+++ api/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const authRoute = require("./routes/auth");
 const postsRoute = require("./routes/posts");
+const usersRoute = require("./routes/users");
 const cors = require("cors");
 
+//ポート番号設定
 const PORT = 5050;
 
 //jsonを受け取れるようにミドルウェア設定
@@ -15,6 +17,7 @@ app.use(cors());
 //各APIのRoute設定
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postsRoute);
+app.use("/api/users", usersRoute);
 
 //サーバー起動
 app.listen(PORT, () => {
