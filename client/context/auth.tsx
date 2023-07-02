fe1d07c0afadab2c90f2a1bebb885c4d548ce608
haruki suziuki <harukiti166@gmail.com>
2023-07-02 16:35:19 +0900

    コンテキスト色々！！

--- client/context/auth.tsx
+++ client/context/auth.tsx
@@ -1,7 +1,12 @@
 import apiClient from "@/lib/apiClient";
-import React, { ReactNode, useContext, useEffect } from "react";
+import React, { ReactNode, useContext, useEffect, useState } from "react";
 
 interface AuthContextType {
+  user: null | {
+    id: number;
+    email: string;
+    username: string;
+  };
   login: (token: string) => void;
   logout: () => void;
 }
@@ -11,6 +16,7 @@ interface AuthProviderProps {
 }
 
 const AuthContext = React.createContext<AuthContextType>({
+  user: null,
   login: () => {},
   logout: () => {},
 });
@@ -20,23 +26,62 @@ export const useAuth = () => {
 };
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
+  //今ログインしているユーザーのデータを保持するstate
+  const [user, setUser] = useState<null | {
+    id: number;
+    email: string;
+    username: string;
+  }>(null);
+
+  //リロードするたびに発火する処理
   //取得したキーをAPIのheaderにつける（こっちはクライアントの処理のためuseEffect可能）
   useEffect(() => {
     //ローカルストレージに保存したトークンのキーを取得する
     const token = localStorage.getItem("auth_token");
-    apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+    if (token) {
+      //APIクライアントのデフォルトのヘッダーにAuthorizationヘッダーを追加するための処理
+      apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      apiClient
+        .get("/users/find")
+        .then((res) => {
+          setUser(res.data.user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }, []);
 
   const login = async (token: string) => {
     localStorage.setItem("auth_token", token);
     apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+    try {
+      apiClient
+        .get("/users/find")
+        .then((res) => {
+          setUser(res.data.user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const logout = () => {
+    //ローカルストレージのtokenキーの値を削除
     localStorage.removeItem("auth_token");
+
+    //APIクライアントのデフォルトのヘッダーに追加したAuthorizationヘッダーを削除するための処理
+    delete apiClient.defaults.headers["Authorication"];
+    setUser(null);
   };
 
   const value = {
+    user,
     login,
     logout,
   };
