a8733bf754bafbbe251e0d5fd63f5b2879ef806c
haruki suziuki <harukiti166@gmail.com>
2023-07-02 11:29:12 +0900

    submit時、リロード時の処理追加

--- client/components/Timeline.tsx
+++ client/components/Timeline.tsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
+import apiClient from "@/lib/apiClient";
+import { PostType } from "@/types/types";
 
 const Timeline = () => {
+  //textareaに入力した内容を保持
+  const [postText, setPostText] = useState<string>("");
+  //最新のPostsデータを配列で保持
+  const [latestPosts, setLatestPosts] = useState<PostType[]>([]);
+
+  //投稿ボタンを押した時の処理
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const newPost = await apiClient.post("/posts/post", {
+        content: postText,
+      });
+
+      //元の配列にnewPostをスプレッド構文で追加
+      setLatestPosts((prevPosts) => [newPost.data, ...prevPosts]);
+
+      setPostText("");
+    } catch (error) {
+      alert("ログインしてください");
+    }
+  };
+
+  //ページをリロードした時にPostを取得するAPIを叩く
+  useEffect(() => {
+    const fetchLatestPosts = async () => {
+      try {
+        const response = await apiClient.get("/posts/get_latest_post");
+        setLatestPosts(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchLatestPosts();
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-100">
       <main className="container mx-auto py-4">
         <div className="bg-white shadow-md rounded p-4 mb-4">
-          <form>
+          <form onSubmit={handleSubmit}>
             <textarea
               className="w-full h-24 p-2 border border-gray-300 rounded resize-none focus:outline-none focus:ring-2 focus:ring-blue-400"
               placeholder="What's on your mind?"
+              value={postText}
+              onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                setPostText(e.target.value)
+              }
             ></textarea>
             <button
               type="submit"
@@ -20,11 +62,9 @@ const Timeline = () => {
           </form>
         </div>
       </main>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      {latestPosts.map((post: PostType) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
 };
