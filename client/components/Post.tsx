2a6b63cfeaa316643fdcb6e85f478a46f05cf334
haruki suziuki <harukiti166@gmail.com>
2023-07-03 13:35:43 +0900

    プロフィールページに飛ぶリンク追加

--- client/components/Post.tsx
+++ client/components/Post.tsx
@@ -1,4 +1,5 @@
 import { PostType } from "@/types/types";
+import Link from "next/link";
 import { type } from "os";
 import React from "react";
 
@@ -9,20 +10,24 @@ type Props = {
 const Post = (props: Props) => {
   const { post } = props;
 
-
-
   return (
     <div className="bg-white shadow-md rounded p-4 mb-4">
       <div className="mb-4">
         <div className="flex items-center mb-2">
-          <img
-            className="w-10 h-10 rounded-full mr-2"
-            src={post.author.profile.profileImageUrl}
-            alt="User Avatar"
-          />
+          {/* 投稿しているユーザのプロフィールページに飛べるようにLink追加 */}
+          <Link href={`/profile/${post.authorId}`}>
+            <img
+              className="w-10 h-10 rounded-full mr-2"
+              src={post.author.profile.profileImageUrl}
+              alt="User Avatar"
+            />
+          </Link>
+
           <div>
             <h2 className="font-semibold text-md">{post.author?.username}</h2>
-            <p className="text-gray-500 text-sm">{new Date(post.createdAt).toLocaleString()}</p>
+            <p className="text-gray-500 text-sm">
+              {new Date(post.createdAt).toLocaleString()}
+            </p>
           </div>
         </div>
         <p className="text-gray-700">{post.content}</p>
